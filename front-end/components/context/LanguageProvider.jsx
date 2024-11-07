72b73d1ea67aad45421d41c3f3e34280e074d5ae
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-07 08:33:56 +0800

    useMemo missing dependency

--- front-end/components/context/LanguageProvider.jsx
+++ front-end/components/context/LanguageProvider.jsx
@@ -4,18 +4,11 @@ import { createContext, useContext, useMemo, useState } from "react";
 const LanguageContext = createContext(null);
 export function LanguageProvider({ children,dictionary }) {
     const [dict,setDict]=useState(dictionary)
-    // const [currentMode,setCurrentMode]=useState(
-    //     {
-    //         id:1,
-    //         text:dictionary?.nav.mode.all,
-    //         color:'#5c5457'
-    //     }
-    // )
     const value=useMemo(()=>(
         {
             dict,setDict
         }
-    ),[dictionary])
+    ),[dict])
   return <LanguageContext.Provider value={value}>{children}</LanguageContext.Provider>;
 }
 export const useLang=()=>{
