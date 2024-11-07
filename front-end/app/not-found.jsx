4ece9ed22d2a6bc1e89f786996504c346097ad50
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-07 08:31:49 +0800

    layout should be there . locale not found is not working . It intrigues me .

--- front-end/app/not-found.jsx
+++ front-end/app/not-found.jsx
@@ -1,10 +1,11 @@
-export default function NotFound(){
-    return <html lang="en">
-    <body
-    >
-      <div className="min-w-[240px] max-w-[700px] m-auto h-screen ">
-      Not found
-      </div>
-    </body>
-  </html>
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>
+        <div className="min-w-[240px] max-w-[700px] m-auto h-screen ">
+          Not found
+        </div>
+      </body>
+    </html>
+  );
 }
--- front-end/components/context/LanguageProvider.jsx
+++ front-end/components/context/LanguageProvider.jsx
@@ -4,16 +4,16 @@ import { createContext, useContext, useMemo, useState } from "react";
 const LanguageContext = createContext(null);
 export function LanguageProvider({ children,dictionary }) {
     const [dict,setDict]=useState(dictionary)
-    const [currentMode,setCurrentMode]=useState(
-        {
-            id:1,
-            text:dictionary?.nav.mode.all,
-            color:'#5c5457'
-        }
-    )
+    // const [currentMode,setCurrentMode]=useState(
+    //     {
+    //         id:1,
+    //         text:dictionary?.nav.mode.all,
+    //         color:'#5c5457'
+    //     }
+    // )
     const value=useMemo(()=>(
         {
-            dict,setDict,currentMode,setCurrentMode
+            dict,setDict
         }
     ),[dictionary])
   return <LanguageContext.Provider value={value}>{children}</LanguageContext.Provider>;
